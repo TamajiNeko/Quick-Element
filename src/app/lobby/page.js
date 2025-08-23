0ccb97fec3597b5447ce865cb306f7382abb1cef
TamajiNeko <classroom3.5.17@gmail.com>
2025-08-23 22:11:45 +0700

    Start Project ꉂ(˵˃ ᗜ ˂˵)

---
+++ src/app/lobby/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Lobby - Quick Element'
+}
+
+export default function lobbyPage() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <p className="text-[2rem]">Loading...</p>
+        </div>
+    )
+}
