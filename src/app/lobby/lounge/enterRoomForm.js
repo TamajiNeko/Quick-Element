ecdb5351cacbd08f57c0ff321e3c89cc7d722696
TamajiNeko <classroom3.5.17@gmail.com>
2025-09-07 23:21:01 +0700

    Day 4 - Little Change

--- src/app/lobby/lounge/enterRoomForm.js
+++ src/app/lobby/lounge/enterRoomForm.js
@@ -10,7 +10,7 @@ const RoomCodeForm = ({ roomCode }) => {
 
   return (
     <form className='EnterForm flex flex-col items-center justify-center bg-white rounded-2xl w-[50vh] h-[25vh] text-[1.5rem]'>
-        <input type="text" name="userName" placeholder="Room Code" className='focus:outline-sky-400 border-[1px] border-[#39b8ff] border-solid w-[80%] h-[22%] rounded-4xl font-black text-center text-black placeholder-slate-400 bg-[white]' required />
+        <input type="text" name="roomCode" placeholder="Room Code" className='focus:outline-sky-400 border-[1px] border-[#39b8ff] border-solid w-[80%] h-[22%] rounded-4xl font-black text-center text-black placeholder-slate-400 bg-[white]' required />
         <br></br>
         <div className="flex w-[80%] h-[20%] justify-center gap-[1rem]">
             <Link href={"/lobby/lounge/seat"} className='w-[50%] rounded-4xl text-white text-center font-[700] bg-[#39b8ff] cursor-pointer'>Create</Link>
