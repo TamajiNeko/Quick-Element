6823930974d32f2e2aacfd2153342c4e03c318f4
TamajiNeko <classroom3.5.17@gmail.com>
2025-10-10 00:07:35 +0700

    Day 19.5 (Ex) - Fixed Typo

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,13 @@ import { useState, useEffect } from 'react';
 export default function Page() {
 
   useEffect(() => {
-    document.title = "Quik Element";
+    document.title = "Quick Element";
   }, []);
   
   return (
     <main>
       <Link href="/lobby" draggable="false" className="felx w-screen h-screen">
-        <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center h-screen"xxx>
           <img src="/logo.png" draggable="false" alt="logo" className="logo w-md"></img>
           <p className="touchStart text-[1.5rem] bottom-[2rem] absolute font-medium text-white">Click to Start</p>
         </div>
