ab8813ac865212e80fc5c0ea5989039a0e1fb584
TamajiNeko <classroom3.5.17@gmail.com>
2025-10-10 16:30:42 +0700

    Day 20.2 (Ex) - CSS Fixing

--- componets/RoomCodePlate.js
+++ componets/RoomCodePlate.js
@@ -5,7 +5,7 @@ export default function RoomCodePlate({ code }) {
   }
   return (
     <div className="flex flex-row gap-[.5rem] items-center justify-center absolute w-screen top-[.75rem]">
-      <p onClick={handleCopy} className="text-[1.5rem] cursor-pointer">Code: {code || 'N/A'}</p>
+      <p onClick={handleCopy} className="text-[2rem] cursor-pointer">Code: {code || 'N/A'}</p>
     </div>
   );
 }
